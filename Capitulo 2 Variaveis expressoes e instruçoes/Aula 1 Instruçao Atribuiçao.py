c2d4d7bffafb21998b22369074a5560160b8067a
seigo00 <felipe.seigo@escola.pr.gov.br>
2026-03-09 11:16:46 -0300

    aula 1 variaveis

---
+++ Capitulo 2 Variaveis expressoes e instruçoes/Aula 1 Instruçao Atribuiçao.py
@@ -0,0 +1,15 @@
+#Variavel é um espaço de memoria que
+# tem um nome e guarda algum dado do meu programa
+nome = ("Felipe")
+sobrenome = ("Donizete")
+numero = 1
+resultado = numero + numero + 5
+print(numero)
+
+#Expressoes sao quaisquer combinações de
+# valor variável e operadores
+resultado = numero + numero + 5
+resultado = (numero * (numero * numero) + 33)
+
+# A instrução print mostra o programacao terminal
+print(numero)
