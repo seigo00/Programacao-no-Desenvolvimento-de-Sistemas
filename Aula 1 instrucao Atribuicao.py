dcf8c01f29a98f5b27f1003712bf5d9ac1597265
seigo00 <felipe.seigo@escola.pr.gov.br>
2026-03-16 11:39:16 -0300

    Aula 1 instrução Atribuição

---
+++ Aula 1 instrucao Atribuicao.py
@@ -0,0 +1,31 @@
+#Variavel é um espaço de memoria que
+# tem um nome e guarda algum dado do meu programa
+nome = ("Felipe")
+sobrenome = ("Donizete")
+numero = 1
+resultado = numero + numero + 5
+print(numero)
+
+#Expressoes sao quaisquer combinações de
+# valor variável e operadores
+resultado = numero + numero + 5
+resultado = (numero * (numero * numero) + 33)
+
+# A instrução print mostra o programacao terminal
+print(numero)
+
+# Precedencia de operadores
+# Parenteses precendia mais alta
+#  Exponenciação
+# Multiplicação e a Divisão
+# Adição e a Subtração
+
+# Concatenação de Strings
+nome = 'Felipe '
+sobrenome = 'Seigo'
+nomeInteiro = nome + sobrenome
+print(nomeInteiro)
+
+# Multiplicação de Strings
+string1 = 'teste ' * 3
+print(string1)
